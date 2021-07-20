178b71ca324c1eebe4e2e5eacb0801e95523c0b1
Mona Lisa <isaac11195@gmail.com>
2021-07-20 19:11:33 -0400

    add components

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
+import Cart from "../components/Cart";
 import { useStoreContext } from "../utils/GlobalState";
-import { UPDATE_PRODUCTS } from "../utils/actions";
+import {
+  REMOVE_FROM_CART,
+  UPDATE_CART_QUANTITY,
+  ADD_TO_CART,
+  UPDATE_PRODUCTS,
+} from '../utils/actions';
 import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
+
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -14,7 +21,7 @@ const [currentProduct, setCurrentProduct] = useState({})
 
 const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-const { products } = state;
+const { products, cart } = state;
 
 useEffect(() => {
   if (products.length) {
@@ -25,32 +32,74 @@ useEffect(() => {
       products: data.products
     });
   }
+  
 }, [products, data, dispatch, id]);
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
 
-          <h2>{currentProduct.name}</h2>
+const addToCart = () => {
+  const itemInCart = cart.find((cartItem) => cartItem._id === id);
 
-          <p>{currentProduct.description}</p>
+  if (itemInCart) {
+    dispatch({
+      type: UPDATE_CART_QUANTITY,
+      _id: id,
+      purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+    });
+  } else {
+    dispatch({
+      type: ADD_TO_CART,
+      product: { ...currentProduct, purchaseQuantity: 1 }
+    });
+  }
+};
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
+const removeFromCart = () => {
+  dispatch({
+    type: REMOVE_FROM_CART,
+    _id: currentProduct._id
+  });
+};
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
-}
+return (
+  <>
+    {currentProduct && cart ? (
+      <div className="container my-1">
+        <Link to="/">
+          ← Back to Products
+        </Link>
+
+        <h2>{currentProduct.name}</h2>
+
+        <p>
+          {currentProduct.description}
+        </p>
+
+        <p>
+          <strong>Price:</strong>
+          ${currentProduct.price}
+          {" "}
+          <button onClick={addToCart}>
+            Add to Cart
+          </button>
+          <button 
+            disabled={!cart.find(p => p._id === currentProduct._id)} 
+            onClick={removeFromCart}
+          >
+            Remove from Cart
+          </button>
+        </p>
+
+        <img
+          src={`/images/${currentProduct.image}`}
+          alt={currentProduct.name}
+        />
+      </div>
+    ) : null}
+    {
+      loading ? <img src={spinner} alt="loading" /> : null
+    }
+    <Cart />
+  </>
+);
+};
 
 export default Detail;
